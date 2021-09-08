c68f9e7d2bbf4bff139ff164070de68314335978
SuRu <33urus@gmail.com>
2021-09-09 00:53:25 +0300

    added docker build

--- src/main/java/com/suru/students/exceptions/EntityNotfoundException.java
+++ src/main/java/com/suru/students/exceptions/EntityNotfoundException.java
@@ -1,8 +1,12 @@
 package com.suru.students.exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
 import java.text.MessageFormat;
 import java.util.UUID;
 
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
 public class EntityNotfoundException extends Exception {
     public EntityNotfoundException(String name, UUID id) {
         super(MessageFormat.format("{0}: {1} not found", name, id));
